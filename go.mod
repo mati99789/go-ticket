7ead8ab91e168cd08ee0969d4339e11d777c03c5
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2026-02-24 19:01:53 +0100

    Feat: Add rate limits test

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	dario.cat/mergo v1.0.2 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/alicebob/miniredis/v2 v2.36.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/containerd/errdefs v1.0.0 // indirect
@@ -59,6 +60,7 @@ require (
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yuin/gopher-lua v1.1.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
---
+++ internal/ratelimit/ratelimiter_test.go
@@ -0,0 +1,48 @@
+package ratelimit
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/redis/go-redis/v9"
+)
+
+func TestRateLimiter(t *testing.T) {
+
+	mr := miniredis.RunT(t)
+	client := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+	limiter := NewRateLimiter(client, 3, 1*time.Minute)
+
+	ctx := context.Background()
+
+	for i := 0; i < 3; i++ {
+		allowed, err := limiter.Allow(ctx, "test")
+		if err != nil {
+			t.Errorf("request %d: unexpected error: %v", i, err)
+		}
+		if !allowed {
+			t.Errorf("request %d: expected allowed, got blocked", i)
+		}
+	}
+	allowed, err := limiter.Allow(ctx, "test")
+	if err != nil {
+		t.Fatalf("4th request: unexpected error: %v", err)
+	}
+	if allowed {
+		t.Errorf("4th request: expected to be blocked, but was allowed")
+	}
+
+	mr.FastForward(time.Minute + time.Second)
+
+	allowed, err = limiter.Allow(ctx, "test")
+	if err != nil {
+		t.Errorf("After window reset: unexpected error: %v", err)
+	}
+	if !allowed {
+		t.Errorf("After window reset: expected allowed, got blocked")
+	}
+}
